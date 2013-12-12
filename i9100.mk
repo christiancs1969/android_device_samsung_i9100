5229bd33cceda57f8cbd8abcfbd323f8bb5b8348
MNT <m.zilius@gmail.com>
2013-12-12 22:26:45 +0200

    Move dalvik-cache from /cache to /data

--- i9100.mk
+++ i9100.mk
@@ -28,7 +28,9 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_LOCALES += hdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240
+    ro.sf.lcd_density=240 \
+    dalvik.vm.dexopt-cache-only=0 \
+    dalvik.vm.dexopt-data-only=1
 
 # Sensors
 PRODUCT_PACKAGES += \
